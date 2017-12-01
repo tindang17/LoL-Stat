54398e0201904a31a1d76fe90dd126cd33bce85c
Tin Dang <tindang1710@gmail.com>
2017-11-30 23:06:26 -0800

    getting data about item with async/await

--- server.js
+++ server.js
@@ -2,26 +2,50 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const request = require('request');
 const rp = require('request-promise');
+// const http2 = require('http2')
 const ENV = process.env.ENV || "development";
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.post('/summoner', (req, res) => {
-  rp(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=${process.env.API_KEY}`)
-  .then(response => {
-    const parsedData = JSON.parse(response);
-    rp(`https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${parsedData.accountId}/recent?api_key=${process.env.API_KEY}`)
-    .then(response => {
-      const parsedData = JSON.parse(response);
-      const { matches } = parsedData;
+async function getItems () {
+  try {
+    let response = await fetch(`https://na1.api.riotgames.com/lol/static-data/v3/items?locale=en_US&api_key=${process.env.API_KEY}`)
+    let data = await response.json();
+    console.log(data);
+  } catch (err) {
+    throw err;
+  }
+}
 
-      // rp(`https://na1.api.riotgames.com/lol/match/v3/matches/${parsedData.gameId}?api_key=${process.env.API_KEY}`)
-      // .then(response => console.log(JSON.parse(response)));
-    });
-  });
+app.post('/summoner', (req, res) => {
+  getItems();
+  const results = [];
+  // results.push(items);
+  // rp(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=${process.env.API_KEY}`)
+  // .then(firstRes => {
+  //   const parsedData = JSON.parse(firstRes);
+  //   results.push(parsedData);
+  //   rp(`https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${parsedData.accountId}/recent?api_key=${process.env.API_KEY}`)
+  //   .then(secRes => {
+  //     const secParsedData = JSON.parse(secRes);
+  //     const { matches } = secParsedData;
+  //     const [ ...latestMatches ] = matches;
+  //     // const [ latestMatches ] = matches;
+  //     // console.log(results);
+  //     for (let i = 0; i <= 1; i++) {
+  //       rp(`https://na1.api.riotgames.com/lol/match/v3/matches/${latestMatches[i].gameId}?api_key=${process.env.API_KEY}`)
+  //       .then(thirdRes => {
+  //         const thirdParsedData = JSON.parse(thirdRes);
+  //         results.push(thirdParsedData);
+  //       });
+  //     }
+  //   });
+  // });
+
 });
 
 app.listen(3001, () => {
